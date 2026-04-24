0b35b0687dd0ac0f3f15104bdc5e5f4ce80d4f5e
lesajackson23 <lesajackson23@gmail.com>
2026-04-24 00:56:48 +0000

    File changes

--- src/components/sv/SVNavbar.jsx
+++ src/components/sv/SVNavbar.jsx
@@ -58,7 +58,7 @@ export default function SVNavbar({ onCTAClick }) {
             <img
               src="https://media.base44.com/images/public/69e96f879840c600a93d0ce5/39f2abf45_Shepherd_verses_herologo_GOLD_STAR_2500_TRANSPARENTBGpng-2.png"
               alt="Shepherd Verses"
-              className="h-10 sm:h-14 md:h-20 w-auto"
+              className="h-14 sm:h-18 md:h-24 w-auto"
             />
           </a>
         </div>
