9b04554baf197cc9e3553750fd87ad2ec2890f5e
lesajackson23 <lesajackson23@gmail.com>
2026-04-26 02:36:17 +0000

    File changes

--- src/components/sv/SVHowItWorks.jsx
+++ src/components/sv/SVHowItWorks.jsx
@@ -40,7 +40,7 @@ export default function SVHowItWorks() {
           </h2>
         </motion.div>
 
-        <div className="max-w-3xl mx-auto">
+        <div className="max-w-3xl mx-auto md:px-16 lg:px-0">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
             {STEPS.map((step, i) => (
               <motion.div
