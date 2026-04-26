666b0199a704ce7a37e92faa23dec66f93bfdbdf
lesajackson23 <lesajackson23@gmail.com>
2026-04-26 02:46:05 +0000

    File changes

--- src/components/sv/SVReviews.jsx
+++ src/components/sv/SVReviews.jsx
@@ -36,7 +36,7 @@ export default function SVReviews() {
   return (
     <section className="relative pt-8 pb-8 md:pt-12 md:pb-12 bg-white">
       <div className="max-w-3xl mx-auto px-10 md:px-16 lg:px-6">
-        <h2 className="font-serif text-2xl md:text-3xl text-[#001C5F] text-center mb-8">
+        <h2 className="font-serif text-lg md:text-3xl text-[#001C5F] text-center mb-8 whitespace-nowrap">
           What Our Members Are Saying
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
